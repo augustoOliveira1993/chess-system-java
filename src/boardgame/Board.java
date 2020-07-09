1746e384fd9a674948b28430a40e8c09030e247c
Augusto Silva <gutim160@gmail.com>
2020-07-09 10:00:02 -0300

    Camada de xadrez e impressão do tabuleiro (Chess layer and printing the board)

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -12,6 +12,14 @@ public class Board {
 		this.pieces = new Piece[this.rows][this.column];
 	}
 
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
 	public int getColumn() {
 		return column;
 	}
@@ -26,5 +34,13 @@ public class Board {
 
 	public void setPieces(Piece[][] pieces) {
 		this.pieces = pieces;
+	}
+	
+	public Piece piece(int row, int column) {
+		return this.pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return this.pieces[position.getRow()][position.getColumn()];
 	}
 }
