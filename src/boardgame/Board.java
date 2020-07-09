3e794d7610f5fc11b7ffb1122c8e83812edf30ad
Augusto Silva <gutim160@gmail.com>
2020-07-09 18:05:12 -0300

    ChessException e ChessPosition (ChessException and ChessPosition)

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,7 +47,7 @@ public class Board {
 	
 	public void placePiece(Piece piece, Position position) {
 		if (this.thereIsApiece(position)) {
-			throw new BoardException("There is alerady a piece on position " + position);
+			throw new BoardException("There is alerady a piece on position  " + position);
 		}
 		this.pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
